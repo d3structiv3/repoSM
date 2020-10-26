f82a0530fdf7ea03ffeb50e978fb3a27d64191c0
Jose Daniel Ramirez Hinojosa <jose.ramirez301@tesh.edu.mx>
2020-10-26 16:58:06 -0600

    SM 26-10-2020

---
+++ VBSM/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    'Autor: destructive'
+    Dim LabelArray(10) As Label
+    Dim TextBoxArray(10) As TextBox
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim i As Integer
+        For i = 1 To 10
+            LabelArray(i) = New Label : TextBoxArray(i) = New TextBox
+            LabelArray(i).Text = "Mensaje " & i
+            LabelArray(i).Width = 120 : TextBoxArray(i).Width = 100
+            LabelArray(i).Height = 30 : TextBoxArray(i).Height = 30
+            TextBoxArray(i).AutoSize = False
+            LabelArray(i).TextAlign = ContentAlignment.MiddleCenter
+            LabelArray(i).BorderStyle = BorderStyle.FixedSingle
+            LabelArray(i).Font = New Font("Calibri", 10, FontStyle.Bold)
+            LabelArray(i).Location = New Point(100, i * 50)
+            TextBoxArray(i).Location = New Point(260, i * 50)
+            Me.Controls.Add(LabelArray(i)) : Me.Controls.Add(TextBoxArray(i))
+        Next
+    End Sub
+End Class
---
+++ VBSM/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBSM")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VBSM")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d8b76deb-756c-4dfa-9d92-9b77e6e6fedd")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
